883b691ce2a790eb69112f0e31cf60b7b314569e
Andie <hetthieherrera@gmail.com>
2026-07-29 20:30:39 -0500

    Cambios dentro de Usuario(agg list)

--- Control medico/src/modelo/Usuario.java
+++ Control medico/src/modelo/Usuario.java
@@ -1,5 +1,6 @@
 package modelo;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Representa un usuario con acceso a la aplicación, vinculado a un
  * {@link Perfil}. Pendiente de definir su uso concreto (login),
@@ -9,18 +10,23 @@ public class Usuario {
 
     private int idUsuario;
     private String nombre;
-    private Perfil perfil;
+    private List<Perfil> perfiles;
     private String email;
 
     public Usuario() {
+	this.perfiles = new ArrayList<>();
     }
 
-    public Usuario(int idUsuario, String nombre, Perfil perfil, String email) {
+    public Usuario(int idUsuario, String nombre, String email) {
         this.idUsuario = idUsuario;
         this.nombre = nombre;
-        this.perfil = perfil;
+        this.perfiles = new ArrayList<>();
         this.email = email;
     }
+
+   public void agregarPerfil(Perfil perfil){
+	this.perfiles.add(perfil);
+   }
 
     public int getIdUsuario() {
         return idUsuario;
@@ -38,12 +44,12 @@ public class Usuario {
         this.nombre = nombre;
     }
 
-    public Perfil getPerfil() {
-        return perfil;
+    public List<Perfil> getPerfiles() {
+        return perfiles;
     }
 
-    public void setPerfil(Perfil perfil) {
-        this.perfil = perfil;
+    public void setPerfil(List<Perfil> perfiles) {
+        this.perfiles = perfiles;
     }
 
     public String getEmail() {
